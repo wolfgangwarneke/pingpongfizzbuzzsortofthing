715b1a1e9a75dc6026ad96d03531118914666e6e
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-06-09 23:17:39 -0700

    change use import from prompt popup to input box form

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 $(document).ready(function() {
-  var howHighToCount = parseInt(prompt('How high should we count?'));
-  var numbersToPrint = [];
-  function tutorialCounter(countTo, firstFactor, secondFactor) {
-    numbersToPrint = [];
-    for (i = 1; i <= countTo; i++) {
-      if ((i % firstFactor === 0) && (i % secondFactor === 0)) {
-        numbersToPrint.push('<em>Pingpong</em> ('+i+')');
-      } else if (i % firstFactor === 0) {
-        numbersToPrint.push('<em>Ping</em> ('+i+')');
-      } else if (i % secondFactor === 0) {
-        numbersToPrint.push('<em>Pong</em> ('+i+')');
-      } else {
-        numbersToPrint.push(i);
+  $('#userNumber').submit(function(event) {
+    $('#tutorialPrint').empty();
+    var howHighToCount = parseInt($('#input').val());
+    var numbersToPrint = [];
+    function tutorialCounter(countTo, firstFactor, secondFactor) {
+      numbersToPrint = [];
+      for (i = 1; i <= countTo; i++) {
+        if ((i % firstFactor === 0) && (i % secondFactor === 0)) {
+          numbersToPrint.push('<em>Pingpong</em> ('+i+')');
+        } else if (i % firstFactor === 0) {
+          numbersToPrint.push('<em>Ping</em> ('+i+')');
+        } else if (i % secondFactor === 0) {
+          numbersToPrint.push('<em>Pong</em> ('+i+')');
+        } else {
+          numbersToPrint.push(i);
+        }
       }
+      numbersToPrint.forEach(function(iteration) {
+        $('#tutorialPrint').append('<li>' + iteration + "</li>");
+      });
     }
-    numbersToPrint.forEach(function(iteration) {
-      $('#tutorialPrint').append('<li>' + iteration + "</li>");
-    });
-  }
-  tutorialCounter(howHighToCount, 3, 5);
+    tutorialCounter(howHighToCount, 3, 5);
+    event.preventDefault();
+  });
 });//end document ready
